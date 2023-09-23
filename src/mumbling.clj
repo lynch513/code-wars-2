e674674f25959b738c68dac231d4b53f69759dc5
solomkin <solomkin@skbkontur.ru>
2023-09-23 23:56:30 +0500

    refactored mumbling

--- src/mumbling.clj
+++ src/mumbling.clj
@@ -3,26 +3,35 @@
             [clojure.string :as str]))
 
 
-(defn accum [s]
-  (->> (loop [count 1
+(defn accum' [s]
+  (->> (loop [count 0
               acc []
               input s]
          (if (empty? input)
            acc
            (let [[head & tail] input
-                 repeated-tail (apply str (repeat (dec count) head))
+                 repeated-tail (apply str (repeat count head))
                  capitalized (str/capitalize (str head repeated-tail))]
              (recur (inc count) (conj acc capitalized) (apply str tail)))))
        (str/join "-")))
 
+(defn accum [s]
+  (->> s
+       (map-indexed (fn [index item]
+                      (->> item
+                           (repeat index)
+                           (apply str)
+                           (str item)
+                           (str/capitalize))))
+       (str/join "-")))
 
 (comment
+  (accum' "test")
   (str/join "-" [1 2 3])
   (accum "TeSt")
   (empty? "")
   (str "" \t)
   (str/capitalize (str \t "t"))
-  (str/upper-case \t)
   (str)
   (str \t "est")
   (apply str (repeat 4 \t))
@@ -34,4 +43,5 @@
   (is (= (accum "NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"))
   (is (= (accum "MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"))
   (is (= (accum "EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"))
-  (is (= (accum "HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")))
+  (is (= (accum "HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"))
+  )
